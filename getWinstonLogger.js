ff04d241452800a2da10be50308bc3b5bd4cedb8
micnekr <misha.nekrasov@gmail.com>
2021-01-22 09:34:15 +0000

    added more logging info

--- getWinstonLogger.js
+++ getWinstonLogger.js
@@ -11,6 +11,7 @@ module.exports = function () {
             // - Write all logs with level `error` and below to `error.log`
             // - Write all logs with level `info` and below to `combined.log`
             //
+            new winston.transports.Console(),
             new winston.transports.File({ filename: './logs/error.log', level: 'error', handleExceptions: true }),
             new winston.transports.File({ filename: './logs/combined.log' }),
         ],
